5a5512c6adb231bded7e4e012c23789fd454e33c
mhull87 <melissahull87@gmail.com>
2020-04-23 23:03:36 -0500

    updated last modified

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 var d = new Date();
-var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-}
+
 document.getElementById("year").textContent = d.getFullYear();
-document.getElementById("date").textContent = new Date().toLocaleDateString("en-US", options);
+
+var lastModified = document.lastModified;
+
+document.getElementById("date").textContent = lastModified;
