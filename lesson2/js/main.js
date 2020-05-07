496c4f035c93e51df50cf1462228333fb013de2d
mhull87 <melissahull87@gmail.com>
2020-05-07 11:48:04 -0500

    added a footer to my design-principles page

---
+++ lesson2/js/main.js
@@ -0,0 +1,7 @@
+var d = new Date();
+
+document.getElementById("year").textContent = d.getFullYear();
+
+var lastModified = document.lastModified;
+
+document.getElementById("date").textContent = lastModified;
