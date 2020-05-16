462ecbad20ddb0bebf53132d979126addf4342bb
mhull87 <melissahull87@gmail.com>
2020-05-16 16:58:39 -0500

    final nedd to validate

--- lesson4/js/main.js
+++ lesson4/js/main.js
@@ -1,4 +1,23 @@
+//hamberger menu
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
-
 }
+
+//copyright year
+var d = new Date();
+document.getElementById("year").textContent = d.getFullYear();
+
+//Current date on footer
+var date = new Date();
+
+var dow = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var day = dow[date.getDay()];
+var number = date.getDate();
+var m = month[date.getMonth()];
+var year = date.getFullYear();
+
+document.getElementById("date").innerHTML = (day + ", " + number + " " + m + " " + year)
+
+
