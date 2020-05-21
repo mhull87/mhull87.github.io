66718ea51b605efa8f32588c81ad603a2aa85046
mhull87 <melissahull87@gmail.com>
2020-05-20 19:51:31 -0500

    pancakes div added for fridays

--- lesson5/js/main.js
+++ lesson5/js/main.js
@@ -18,6 +18,13 @@ var number = date.getDate();
 var m = month[date.getMonth()];
 var year = date.getFullYear();
 
-document.getElementById("date").innerHTML = (day + ", " + number + " " + m + " " + year)
+document.getElementById("date").innerHTML = (day + ", " + number + " " + m + " " + year);
 
+//Preston Pancake banner
+var pancakesDay = new Date();
+if (pancakesDay.getDay() == [5]) {
+    document.getElementById("banner").innerHTML = ("Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.");
 
+} else {
+    document.getElementById("banner").style.display = "none";
+}
