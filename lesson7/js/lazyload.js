9c2dab2bd386add43b9bd8172b95645b3112c400
mhull87 <melissahull87@gmail.com>
2020-06-05 22:12:02 -0500

    asdfghj

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -3,8 +3,8 @@ const imagesToLoad = document.querySelectorAll("img[data-src]");
 
 //telling it when to load
 const imgOptions = {
-    threshold: 1,
-    rootMargin: "0px 0px -55px 0px"
+    threshold: 0,
+    rootMargin: "0px 0px 5px 0px"
 };
 
 //move from data-src to src and remove data-src attribute
