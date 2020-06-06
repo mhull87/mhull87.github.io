2dd6949d184b832bde291e34a112fcfbc25a73cf
mhull87 <melissahull87@gmail.com>
2020-06-06 16:58:09 -0500

    fixes to images and windchill

--- lesson6/js/main.js
+++ lesson6/js/main.js
@@ -8,21 +8,19 @@ var d = new Date();
 document.getElementById("year").textContent = d.getFullYear();
 
 //Current date on footer
-var date = new Date();
 
 var dow = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-var day = dow[date.getDay()];
-var number = date.getDate();
-var m = month[date.getMonth()];
-var year = date.getFullYear();
+var day = dow[d.getDay()];
+var number = d.getDate();
+var m = month[d.getMonth()];
+var year = d.getFullYear();
 
 document.getElementById("date").innerHTML = (day + ", " + number + " " + m + " " + year);
 
 //Preston Pancake banner
-var pancakesDay = new Date();
-if (pancakesDay.getDay() == [5]) {
+if (d.getDay() == [5]) {
     document.getElementById("banner").innerHTML = ("Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.");
 
 } else {
--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -2,7 +2,7 @@
 
     var ftemp = document.getElementById("temp").innerHTML;
     var wspeed = document.getElementById("speed").innerHTML;
-    var wchill = 35.74 + (0.6215 * ftemp) - Math.pow((35.75 * wspeed), .16) + Math.pow((.4275 * ftemp * wspeed), .16); 
+    var wchill = 35.74 + (0.6215 * ftemp) - 35.75 * Math.pow((wspeed), .16) + .4275 * ftemp * Math.pow((wspeed), .16); 
 
 
     if (ftemp <= 50 && wspeed >= 3) {
