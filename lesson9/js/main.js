1be37655af92b6d52ae97f23e92e4d1427406eb6
mhull87 <melissahull87@gmail.com>
2020-06-18 11:05:54 -0500

    made week 9 folder

---
+++ lesson9/js/main.js
@@ -0,0 +1,33 @@
+//hamberger menu
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+//copyright year
+var d = new Date();
+document.getElementById("year").textContent = d.getFullYear();
+
+//Current date on footer
+
+var dow = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var day = dow[d.getDay()];
+var number = d.getDate();
+var m = month[d.getMonth()];
+var year = d.getFullYear();
+
+document.getElementById("date").innerHTML = (day + ", " + number + " " + m + " " + year);
+
+//Preston Pancake banner
+if (d.getDay() == [5]) {
+    document.getElementById("banner").innerHTML = ("Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.");
+
+} else {
+    document.getElementById("banner").style.display = "none";
+}
+
+//Storm Severity Range Slider
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
---
+++ lesson9/js/windchill.js
@@ -0,0 +1,14 @@
+
+
+    var ftemp = document.getElementById("temp").innerHTML;
+    var wspeed = document.getElementById("speed").innerHTML;
+    var wchill = 35.74 + (0.6215 * ftemp) - 35.75 * Math.pow((wspeed), .16) + .4275 * ftemp * Math.pow((wspeed), .16); 
+
+
+    if (ftemp <= 50 && wspeed >= 3) {
+        document.getElementById("chill").innerHTML = Math.round(wchill);
+    }
+    else {
+        document.getElementById("chill").innerHTML = "N/A";
+    }
+
