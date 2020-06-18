a66d9e5d79bd6ddc45dcce8b4b13cdd3eb59849f
mhull87 <melissahull87@gmail.com>
2020-06-18 16:27:56 -0500

    mjnhbgvfcx

--- lesson9/js/main.js
+++ lesson9/js/main.js
@@ -31,3 +31,24 @@ if (d.getDay() == [5]) {
 function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
+
+//JSON for home page
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(function (response) {
+    return response.json();
+})
+
+.then(function (jsonObject) {
+    const towns = jsonObject['towns'];
+    for (let i = 0; i < towns.length; i++) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+
+        h2.textContent = towns[i].name;
+
+        card.appendChild(h2);
+
+        document.querySelector('div.cards').appendChild(card);
+    }
+})
