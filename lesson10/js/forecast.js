0ba617a26e04df02a70c8543299425d2e0ae081f
mhull87 <melissahull87@gmail.com>
2020-06-26 13:00:36 -0500

    added lesson ten to portal

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -7,13 +7,15 @@ fetch(apiURL)
         var current = jsObject.weather[0].main;
         var ftemp = jsObject.main.temp;
         var wspeed = jsObject.wind.speed;
+        var hum = jsObject.main.humidity;
         var wchill = 35.74 + (0.6215 * ftemp) - 35.75 * Math.pow((wspeed), .16) + .4275 * ftemp * Math.pow((wspeed), .16); 
         document.getElementById('currently').textContent = current;
         document.getElementById('currentTemp').textContent = Math.round(ftemp);
+        document.getElementById('humidity').textContent = hum;
         document.getElementById('speed').textContent = Math.round(wspeed);
 
         if (ftemp <= 50 && wspeed >= 3) {
-            document.getElementById('chill').innerHTML = Math.round(wchill);
+            document.getElementById('chill').innerHTML = Math.round(wchill) + "&#8457;";
         }
         else {
             document.getElementById('chill').innerHTML = 'N/A';
@@ -27,25 +29,21 @@ fetch(getURL)
     .then((jsObject) => {
         console.log(jsObject);
 
-        const fivedayforecast = jsObject.list.filter(x => x.dt_txt.includes('15:00:00'));
+        const fivedayforecast = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
 
         const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
         let day = 0;
         fivedayforecast.forEach(forecast => {
             let d = new Date(forecast.dt_txt);
+            const imagesrc = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+            const desc = forecast.weather[0].description;
+            
             document.getElementById(`forecast${day+1}`).textContent = Math.round(forecast.main.temp);
             document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
- //           document.getElementById(`img${day+1}`).innerHTML = forecast.weather[0].icon;
+            document.getElementById(`img${day+1}`).setAttribute('src', imagesrc);
+            document.getElementById(`img${day+1}`).setAttribute('alt', desc);
+ 
             day++;
-
-
         });
- //       const desc = jsObject.weather[0].description;
-  //      const icon = document.querySelector('img');
-  //      const imagesrc = `https://openweathermap.org/img/w/${jsObject.list.weather[0].icon}.png`;
-  //      icon.setAttribute('src', imagesrc);
-  //      icon.setAttribute('alt', desc);
-
-
     });
