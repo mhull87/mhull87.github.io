55f103d9f7242ece04979d9eae469d953b7e97d4
mhull87 <melissahull87@gmail.com>
2020-06-30 20:28:24 -0500

    made lesson 11 folder and added fish haven and soda springs pages

---
+++ lesson11/js/lazyload.js
@@ -0,0 +1,40 @@
+//get images with data-src attribute
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+//telling it when to load
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 5px 0px"
+};
+
+//move from data-src to src and remove data-src attribute
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+};
+
+
+if ("IntersectionObserver" in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+//if supported load images    
+imagesToLoad.forEach((img) => {
+    imgObserver.observe(img);
+});
+
+//if not supported load all images
+} else {
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+});
+
+}
