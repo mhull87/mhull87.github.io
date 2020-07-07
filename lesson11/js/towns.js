aac3a41a8c23f1721d6e29465690496763615d7a
mhull87 <melissahull87@gmail.com>
2020-07-06 21:39:12 -0500

    kjhgf

--- lesson11/js/towns.js
+++ lesson11/js/towns.js
@@ -1,19 +1,17 @@
 let townid = " ";
 let name = document.querySelector('.active').textContent;
 
-if (name == "Preston")  {
+if (name == "Preston") {
     townid = "id=5604473";
-}
-else if (name == "Soda Springs") {
+} else if (name == "Soda Springs") {
     townid = "id=5607916";
-}
-else if (name == "Fish Haven") {
+} else if (name == "Fish Haven") {
     townid = "lat=42.0380399&lon=-111.4048681";
 }
 
-const apiURLSS = 'https://api.openweathermap.org/data/2.5/weather?' + townid + '&units=imperial&APPID=9e82453a440594b3cdfb2221e87e0b94';
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?' + townid + '&units=imperial&APPID=9e82453a440594b3cdfb2221e87e0b94';
 
-fetch(apiURLSS)
+fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
 
@@ -21,7 +19,7 @@ fetch(apiURLSS)
         var ftemp = jsObject.main.temp;
         var wspeed = jsObject.wind.speed;
         var hum = jsObject.main.humidity;
-        var wchill = 35.74 + (0.6215 * ftemp) - 35.75 * Math.pow((wspeed), .16) + .4275 * ftemp * Math.pow((wspeed), .16); 
+        var wchill = 35.74 + (0.6215 * ftemp) - 35.75 * Math.pow((wspeed), .16) + .4275 * ftemp * Math.pow((wspeed), .16);
         document.getElementById('currently').textContent = current;
         document.getElementById('currentTemp').textContent = Math.round(ftemp);
         document.getElementById('humidity').textContent = hum;
@@ -29,15 +27,14 @@ fetch(apiURLSS)
 
         if (ftemp <= 50 && wspeed >= 3) {
             document.getElementById('chill').innerHTML = Math.round(wchill) + "&#8457;";
-        }
-        else {
+        } else {
             document.getElementById('chill').innerHTML = 'N/A';
         }
     });
 
-const getURLSS = 'https://api.openweathermap.org/data/2.5/forecast?' + townid + '&units=imperial&APPID=9e82453a440594b3cdfb2221e87e0b94';
+const getURL = 'https://api.openweathermap.org/data/2.5/forecast?' + townid + '&units=imperial&APPID=9e82453a440594b3cdfb2221e87e0b94';
 
-fetch(getURLSS)
+fetch(getURL)
     .then((response) => response.json())
     .then((jsObject) => {
 
@@ -50,12 +47,49 @@ fetch(getURLSS)
             let d = new Date(forecast.dt_txt);
             const imagesrc = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
             const desc = forecast.weather[0].description;
-            
+
             document.getElementById(`forecast${day+1}`).textContent = Math.floor(forecast.main.temp);
             document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
             document.getElementById(`img${day+1}`).setAttribute('src', imagesrc);
             document.getElementById(`img${day+1}`).setAttribute('alt', desc);
- 
+
             day++;
         });
+    });
+
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+    .then(function(response) {
+        return response.json();
+    })
+
+    .then(function(jsonObject) {
+
+        const towns = jsonObject['towns'];
+
+        towns.forEach(town => {
+
+            if (town.name == name) {
+
+                var events = document.createElement('section');
+                var header = document.createElement('h3')
+                var list = document.createElement('ul');
+                var eventitems = town.events;
+
+                eventitems.forEach(eventitem => {
+                    var li = document.createElement('li');
+                    li.textContent = eventitem;
+                    list.appendChild(li);
+                });
+
+                header.textContent = `Upcomming Events`;
+
+                events.appendChild(header);
+                events.appendChild(list);
+
+                document.querySelector('div.events').appendChild(events);
+            }
+        });
+
     });
